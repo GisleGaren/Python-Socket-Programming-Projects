74e3cf181d72b2b8a92990c7dd2edd57ab772b97
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2023-02-24 22:07:40 +0100

    Endring slik at jeg kan redigere resten på stasjonære pcen

--- Lab2/server.py
+++ Lab2/server.py
@@ -9,7 +9,7 @@ import _thread as thread
 import time
 import sys
 
-#this is too keep all the newly joined connections! 
+#this is too keep all the newly joined connections and store them as an array. 
 all_client_connections = []
 
 def now():
@@ -24,16 +24,14 @@ def handleClient(connection, addr):
 	"""
 	#this is where we broadcast everyone that a new client has joined
 	print(f"[NEW CONNECTION] {addr} connected")
-	### Write your code here ###
 
-	# append this this to the list for broadcast
+	# append function takes in the connection parameter
 	all_client_connections.append(connection)
 	# create a message to inform all other clients 
 	# that a new client has just joined.
 	broadcast(connection, f"Client {addr} joined.")
 	welcomeMessage = "Welcome to the chat!"
 	connection.send(welcomeMessage.encode())
-	### Your code ends here ###
 
 	try:
 		while True: # A loop that goes on and on...
@@ -73,30 +71,23 @@ def main():
 	creates a server socket, listens for new connections,
 	and spawns a new thread whenever a new connection join
 	"""
-	serverPort = 12000
-	server_ip = socket.gethostbyname(socket.gethostname())
-	serverSocket = socket.socket(AF_INET,SOCK_STREAM) # Create a server socket that client sockets can connect to
+	serverPort = 12000  # We choose a random port that is usually not used, which are ports in the 4 digit range and above.
+	server_ip = socket.gethostbyname(socket.gethostname()) # This line of code will get the hostname of a server regardless of where we have connected, so that we don't ahve to hardcode a specific ip each time.
+	serverSocket = socket.socket(AF_INET,SOCK_STREAM) # Create a server socket that client sockets can connect to. AF_INET means that we are dealing with ipv4 connections and sock_stream means that 
 	try:
-		# Use the bind function wisely!
-		### Write your code here ###
-		serverSocket.bind((server_ip,serverPort))
-		### Your code ends here ###
-		
+		serverSocket.bind((server_ip,serverPort)) # This function binds the serverSocket object to the specified ip address of the local machine and the server will listen in on that ip address and specific port number 12000.
 	except: 
-		print("Bind failed. Error : ")
-		sys.exit()
-	serverSocket.listen(10)
-	print ('The server is ready to receive')
-	while True:
-		### Write your code here ###
+		print("Bind failed. Error : ")   # If the serverSocket is unable to bind itself to the port and ip, it will return an error in the terminal.
+		sys.exit()  # Terminate the program when an error occurs that cannot be handled properly.
+	serverSocket.listen(10) # The serverSocket listens in on incoming connections from potential clients and can take upto 10 clients simultaneously, hence the paramter 10.
+	print ('The server is ready to receive') # If the serverSocket successfully binds to the ip and port number. Print ready to receive.
+	while True: # continuous loop
 		connectionSocket, addr = serverSocket.accept()  # Is a blocking method call so that it waits until the server accepts the client connection before moving to the next line of code.
-		# When the client connects, the server creates a new socket representing the connection and the address of the corresponding client addr which consists of (host, port)
-		### You code ends here ###
-		 
-		print('Server connected by ', addr) 
-		print('at ', now())
+		# When the client connects, the server creates a new socket (connectionSocket) representing the connection (which is used for sending and receiving data between the client and the server) and the address of the corresponding client addr which consists of (host, port)
+		print('Server connected by ', addr) # We print which address connected to server when the client connects.
+		print('at ', now()) # prints out what time the client connected.
 		thread.start_new_thread(handleClient, (connectionSocket,addr)) # We start a new thread that runs the handleClient() function where we pass connectionSocket and addr as arguments.
-	serverSocket.close()
+	serverSocket.close()  # If 
 
 if __name__ == '__main__':
 	main()
