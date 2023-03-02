a0927372c184c935d2d206efbb482bf5d03a4023
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2023-03-02 16:31:39 +0100

    server1.py works as it should now. The problem persisted with the code not being able to identify the path. The address is now displayed as a comment in server1.py Line 2.

---
+++ Oblig/client2.py
@@ -0,0 +1,31 @@
+import sys
+from socket import *
+
+# Get command line arguments
+server_host = sys.argv[1]
+server_port = int(sys.argv[2])
+filename = sys.argv[3]
+
+# Create TCP socket and connect to server
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((server_host, server_port))
+
+# Send GET request to server
+request = f"GET /{filename} HTTP/1.1\r\nHost: {server_host}\r\n\r\n"
+print(request)
+try:
+    client_socket.send(request.encode())
+except:
+    print("Error sending GET Request to the server. Please try again later...")
+
+try:
+    # Receive response from server
+    response = client_socket.recv(4096)
+    # Print server response
+    print(response.decode())
+except:
+    print("Error receiving response from the server. Socket closed")
+    sys.exit()
+
+# Close socket
+client_socket.close()
--- Oblig/server1.py
+++ Oblig/server1.py
@@ -1,13 +1,16 @@
 #import socket module
+# Write http://10.47.24.207:12000/Oblig/index.html depending on the ip address.
 from socket import *
+import socket # We imported socket so that we can get the socket.gethostbyname(socket.gethostname()) code so that we don't need to hardcode the private ip address
 import sys # In order to terminate the program
-serverSocket = socket(AF_INET, SOCK_STREAM) 
+serverSocket = socket.socket(AF_INET, SOCK_STREAM) 
 
 #Prepare a sever socket
 #Write your code here
 serverPort = 12000  # We choose a random port that is usually not used, which are ports in the 4 digit range and above.
 server_ip = socket.gethostbyname(socket.gethostname()) # This line of code will get the hostname of a server regardless of where we have connected, so that we don't ahve to hardcode a specific ip each time.
-serverSocket = socket.socket(AF_INET,SOCK_STREAM) # Create a server socket that client sockets can connect to. AF_INET means that we are dealing with ipv4 connections and sock_stream means that we are connecting via TCP protocol
+print(server_ip)
+# Create a server socket that client sockets can connect to. AF_INET means that we are dealing with ipv4 connections and sock_stream means that we are connecting via TCP protocol
 try:
 	serverSocket.bind((server_ip, serverPort))
 except:
@@ -24,18 +27,19 @@ while True:
 		connectionSocket, addr = serverSocket.accept()
 		#End of your code
 		message = connectionSocket.recv(1024).decode() #Write your code here #End of your code 
-		filename = message.split()[1] # This line splits the message by its spaces for example: "I like cats" turns into {"I", "like", "cats"}
+		filename = message.split()[1] # This line splits the message by its spaces for example: "I like cats" turns into {"I", "like", "cats"}. If we do print(filename) the terminal returns "/index.html"
 		f = open(filename[1:]) # [1:] will return all elements except for the first one so example "/index.html" will be "index.html" because the 0th element is ignored
-		outputdata = f.read() #Write your code here #End of your code 
+		outputdata = f.read() # Write your code here #End of your code 
 
 		#Send one HTTP header line into socket
 		#Write your code here
-		msg = "HTTP/1.1 200 OK\r\n\r\n"
-		connectionSocket.send(msg.encode())
+		#Here the we are sending a message to a client indicating that the HTTP GET request method was a success which is what the status code "200" means.
+		msg = "HTTP/1.1 200 OK\r\n\r\n" # It contains the message HTTP version 1.1 which is what is currently mostly in use worldwide and 200 OK meaning success.
+		connectionSocket.send(msg.encode()) # Ultimately, the server tells the client that the server is ready to send file contents.
 		#End of your code
 
 		#Send the content of the requested file to the client 
-		for i in range(0, len(outputdata)):
+		for i in range(0, len(outputdata)): 
 			connectionSocket.send(outputdata[i].encode()) 
 		connectionSocket.send("\r\n".encode())
 		connectionSocket.close()
@@ -43,13 +47,14 @@ while True:
 	except IOError:
 		#Send response message for file not found
     	#Write your code here
-
+		connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
     	#End of your code
 		
+		connectionSocket.send("<html><head></head><body><h1>404 Not Found Test test</h1></body></html>\r\n".encode())
 		#Close client socket
         
         #Write your code here
-		
+		connectionSocket.close()
 		#End of your code
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data
